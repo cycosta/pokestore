af5f080439ed5070eaced5c7f0988c7dc9d172f5
Cynthia Costa <cyxcosta@gmail.com>
2020-08-21 11:10:44 -0300

    Implement theme change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,28 @@
 import React, { useState, useEffect } from 'react'
 
 import freezeScroll from '../assets/javascript/freezeScroll'
+import storeThemes from '../assets/javascript/theme'
 
 function Header() {
+  const { standard, fire, water, grass } = storeThemes
+
   const [menuOpen, setMenuOpen] = useState(false)
 
-  useEffect(() => freezeScroll(menuOpen))
+  const [theme, setTheme] = useState(standard)
+
+  function changeTheme(theme) {
+    document.documentElement.style.setProperty('--theme', theme)
+    setMenuOpen(false)
+  }
+
+  useEffect(() => freezeScroll(menuOpen), [menuOpen])
+
+  useEffect(() => changeTheme(theme), [theme])
 
   return (
     <header className="header">
       <div className="header__content">
-        <h1 className="header__logo">
+        <h1 className="header__logo" onClick={() => setTheme(standard)}>
           pokestore
         </h1>
         <button onClick={() => setMenuOpen(!menuOpen)} className={`header__menu ${menuOpen ? 'header__menu--close' : ''}`}>
@@ -18,9 +30,9 @@ function Header() {
         </button>
         <nav className={`header__nav ${menuOpen ? 'header__nav--open' : ''}`}>
           <ul className="header__list">
-            <li className="header__item">Fire Store</li>
-            <li className="header__item">Water Store</li>
-            <li className="header__item">Grass Store</li>
+            <li className="header__item" onClick={() => setTheme(fire)}>Fire Store</li>
+            <li className="header__item" onClick={() => setTheme(water)}>Water Store</li>
+            <li className="header__item" onClick={() => setTheme(grass)}>Grass Store</li>
           </ul>
         </nav>
       </div>
