d4153398f566d757ed4ccd4733c4b885f052746d
Cynthia Costa <cyxcosta@gmail.com>
2020-08-21 18:29:40 -0300

    Updates

---
+++ precache-manifest.1de30966fd18b50e9477296a91e2be31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d35f2bd271b17e594498197c2a7838d",
+    "url": "/pokestore/index.html"
+  },
+  {
+    "revision": "91ac0317b419b04c005b",
+    "url": "/pokestore/static/css/main.f2dc3cbe.chunk.css"
+  },
+  {
+    "revision": "cc636480525937d2f156",
+    "url": "/pokestore/static/js/2.29d88a8a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokestore/static/js/2.29d88a8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91ac0317b419b04c005b",
+    "url": "/pokestore/static/js/main.3599180e.chunk.js"
+  },
+  {
+    "revision": "f9c9c69c191cfd40181c",
+    "url": "/pokestore/static/js/runtime-main.bf0ea42c.js"
+  }
+]);
